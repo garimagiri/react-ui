60dd884b2f285324a981cf34b24f1f20924986b8
Goswami_labs <89575497+garimagiri@users.noreply.github.com>
2023-04-12 21:46:22 +0530

    initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+function Footer() {
+  return (
+    <div>
+      <div class="flex">
+      <img src="../images/img_left.png" class=" mr-3" alt="Logo" />
+      <img src="../images/img_right.png" class=" mr-3" alt="Logo" />
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/components/Section.js
@@ -0,0 +1,17 @@
+function Section() {
+  return (
+    <div>
+      <div>This is a sample</div>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <div><input type="text" placeholder="enter email"></input></div>
+      <div class="flex">
+      <img src="../images/google.png" class="h-8 mr-3" alt="Logo" />
+      <img src="../images/disney.png" class="h-8 mr-3" alt="Logo" />
+      <img src="../images/microsoft.png" class="h-8 mr-3" alt="Logo" />
+      <img src="../images/sony.png" class="h-8 mr-3" alt="Logo" />
+      </div>
+    </div>
+  );
+}
+
+export default Section;
